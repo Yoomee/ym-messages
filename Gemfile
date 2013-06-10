6bd56d920ad617c1e335c18e9355433218d3ded8
Edward Andrews-Hodgson <edward@yoomee.com>
2013-06-10 16:16:18 +0100

    Single quotes

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
-source "https://yoomee:wLjuGMTu30AvxVyIrq3datc73LVUkvo@gems.yoomee.com"
-source "http://rubygems.org"
+source 'https://yoomee:wLjuGMTu30AvxVyIrq3datc73LVUkvo@gems.yoomee.com'
+source 'http://rubygems.org'
 
 # Declare your gem's dependencies in ym_messages.gemspec.
 # Bundler will treat runtime dependencies like base dependencies, and
--- lib/generators/ym_messages/install_generator.rb
+++ lib/generators/ym_messages/install_generator.rb
@@ -4,30 +4,30 @@ module YmMessages
       include YmCore::Generators::Migration
       include YmCore::Generators::Ability
 
-      source_root File.expand_path("../templates", __FILE__)
-      desc "Installs YmMessages."
+      source_root File.expand_path('../templates', __FILE__)
+      desc 'Installs YmMessages.'
 
       def manifest
         # examples ->
-        # copy_file "models/page.rb", "app/models/page.rb"
+        # copy_file 'models/page.rb', 'app/models/page.rb'
         # if should_add_abilities?('Page')
-        #   add_ability(:open, "can :show, Page, :draft => false")
+        #   add_ability(:open, 'can :show, Page, :draft => false')
         # end
-        # try_migration_template "migrations/create_pages.rb", "db/migrate/create_pages"
+        # try_migration_template 'migrations/create_pages.rb', 'db/migrate/create_pages'
    
         Dir[File.dirname(__FILE__) + '/templates/models/*.rb'].each do |file_path|
-          file_name = file_path.split("/").last          
+          file_name = file_path.split('/').last          
           copy_file "models/#{file_name}", "app/models/#{file_name}"
         end
         
         Dir[File.dirname(__FILE__) + '/templates/controllers/*.rb'].each do |file_path|
-          file_name = file_path.split("/").last          
+          file_name = file_path.split('/').last          
           copy_file "controllers/#{file_name}", "app/controllers/#{file_name}"
         end
         
         # Migrations must go last
         Dir[File.dirname(__FILE__) + '/templates/migrations/*.rb'].each do |file_path|
-          file_name = file_path.split("/").last
+          file_name = file_path.split('/').last
           try_migration_template "migrations/#{file_name}", "db/migrate/#{file_name.sub(/^\d+\_/, '')}"
         end
       end
