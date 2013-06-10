74d7095e365215899ac4b166b6d57f5d5d131fcd
Edward Andrews-Hodgson <edward@yoomee.com>
2013-06-10 13:45:31 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,15 @@
 Rails.application.routes.draw do
+
+  resources :messages, :only => [:new, :create]
+
+  resources :users, :only => [] do
+    resources :messages, :only => [:new] do
+      collection do
+        put 'update_settings'
+      end
+    end
+  end
+
+  resources :message_threads, :only => [:index, :show], :path => 'messages'
+
 end
