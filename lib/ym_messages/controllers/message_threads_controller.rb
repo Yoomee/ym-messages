f6798d0ec785ac84666dbdd947cae7094c692cc4
Matt Atkins <matt@yoomee.com>
2013-10-14 16:56:14 +0100

    Allow admins to view the inboxes of others

--- lib/ym_messages/controllers/message_threads_controller.rb
+++ lib/ym_messages/controllers/message_threads_controller.rb
@@ -2,6 +2,7 @@ module YmMessages::MessageThreadsController
 
   def self.included(base)
     base.load_and_authorize_resource
+    base.before_filter :set_user
   end
   
   def all
@@ -9,13 +10,22 @@ module YmMessages::MessageThreadsController
   end
 
   def index
-    @message_threads = current_user.threads.order('updated_at DESC').paginate(:per_page => 50, :page => params[:page])
+    @message_threads = @user.threads.order('updated_at DESC').paginate(:per_page => 50, :page => params[:page])
   end
   
   def show
-    @message_thread.set_read!(current_user)
+    @message_thread.set_read!(current_user) if current_user == @user
     @messages = @message_thread.messages.reorder('messages.created_at DESC').paginate(:per_page => 10, :page => params[:page])
     @last_message = @messages.first
+  end
+  
+  private
+  def set_user
+    if current_user.is_admin? && params[:user_id].present?
+      @user = User.find(params[:user_id])
+    else
+      @user = current_user
+    end
   end
 
 end
